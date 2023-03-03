3215f0ad7078fe0003b832d35916e1d3e590caa2
Randy Chu <randychu7@gmail.com>
2023-03-03 14:06:24 -0600

    lol

---
+++ albums_migration.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+
+                       id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                       artist VARCHAR(50) DEFAULT 'NONE',
+                       name VARCHAR(100) NOT NULL,
+                       release_date date,
+                       sales DECIMAL(4,2),
+                       genre VARCHAR(50),
+                       PRIMARY KEY (id)
+);
---
+++ mycontacts.sql
@@ -0,0 +1,10 @@
+USE zenith_database_db
+
+CREATE TABLE books (
+
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        author_first_name VARCHAR(50) DEFAULT 'NONE',
+                        author_last_name  VARCHAR(100) NOT NULL,
+                        content TEXT NOT NULL,
+                        PRIMARY KEY (id)
+);
