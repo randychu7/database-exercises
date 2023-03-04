5819341fdc2b03cb4a6d091065162568c570bb41
Randy Chu <randychu7@gmail.com>
2023-03-03 18:23:30 -0600

    lol

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,6 +2,7 @@ USE codeup_test_db;
 
 CREATE TABLE albums (
 
+
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        artist VARCHAR(50) DEFAULT 'NONE',
                        name VARCHAR(100) NOT NULL,
