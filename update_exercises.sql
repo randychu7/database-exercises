45179b5d9aefe9dd81971097853697004eb27a31
Randy Chu <randychu7@gmail.com>
2023-03-06 10:59:57 -0600

    lol

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,7 +1,8 @@
 USE codeup_test_db;
 
 SELECT * from albums;
-UPDATE albums SET sales = sales * 10;
+UPDATE albums
+SET sales = sales * 10;
 
 SELECT name from albums where release_date > 1980;
 UPDATE albums SET release_date = 1800 WHERE  release_date > 1980;
