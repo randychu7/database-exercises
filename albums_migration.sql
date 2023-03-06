f249274ffb4457bf76b02a50b8326f2be9367b73
Randy Chu <randychu7@gmail.com>
2023-03-06 10:19:28 -0600

    finished exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,16 @@
 USE codeup_test_db;
-
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
 
 
                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
                        artist VARCHAR(50) DEFAULT 'NONE',
                        name VARCHAR(100) NOT NULL,
-                       release_date date,
-                       sales DECIMAL(4,2),
+                       release_date INT UNSIGNED,
+                       sales FLOAT,
                        genre VARCHAR(50),
                        PRIMARY KEY (id)
 );
 
-SELECT  * FROM albums;
+# SELECT  * FROM albums WHERE artist = 'Pink Floyd';
 
---
+++ select_exercises.sql
@@ -0,0 +1,13 @@
+USE codeup_test_db;
+
+SELECT  *  FROM albums WHERE artist = 'Pink Floyd';
+
+SELECT release_date FROM albums WHERE albums.name = 'Sgt. Pepper''s Lonely Hearts Club Band';
+
+SELECT genre FROM albums WHERE name = 'Nevermind';
+
+SELECT name from albums WHERE release_date = '1990';
+
+SELECT name from albums WHERE sales >= 20;
+
+SELECT name from albums where genre = 'Rock';
