aeae02b0962a414f8a8049827e1e235c82c9e837
Randy Chu <randychu7@gmail.com>
2023-03-13 16:52:04 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -14,7 +14,6 @@ FROM salaries
          JOIN departments ON departments.dept_no = dept_manager.dept_no
          WHERE salaries.to_date = '9999-01-01'
         ORDER BY first_name desc;
-
 #
 SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ' , employees.last_name) AS 'Department Manager'
 FROM departments
