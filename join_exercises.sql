8fc3ca56355fff8844559dee735ca223c5d75347
Randy Chu <randychu7@gmail.com>
2023-03-13 16:52:23 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -53,7 +53,6 @@ GROUP BY departments.dept_name, Department_Manager, salaries.salary
 ORDER BY `Department Name` asc;
 
 
-
 # SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name, s.salary
 # FROM salaries AS s
 #          JOIN dept_manager AS dm ON dm.emp_no = s.emp_no
