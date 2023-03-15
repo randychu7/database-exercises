6a1e5394179f5e042cc29e5813a3c9662d614c23
Randy Chu <randychu7@gmail.com>
2023-03-14 19:10:15 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -50,11 +50,3 @@ JOIN dept_manager on employees.emp_no = dept_manager.emp_no
 WHERE salaries.to_date = '9999-01-01' AND dept_manager.to_date = '9999-01-01'
 GROUP BY departments.dept_name, Department_Manager, salaries.salary
 ORDER BY `Department Name` asc;
-
-# SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS manager_name, s.salary
-# FROM salaries AS s
-#          JOIN dept_manager AS dm ON dm.emp_no = s.emp_no
-#          JOIN employees AS e ON e.emp_no = dm.emp_no
-#          JOIN departments AS d ON d.dept_no = dm.dept_no
-# WHERE s.to_date = '9999-01-01' AND dm.to_date = '9999-01-01'
-# GROUP BY d.dept_name, manager_name, s.salary;
