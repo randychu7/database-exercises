64217625f18def0728bf8439bb9d15b66ec322ed
Randy Chu <randychu7@gmail.com>
2023-03-14 19:09:58 -0500

    finished exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -41,7 +41,6 @@ FROM titles
 GROUP BY titles.title
 ORDER BY Total DESC;
 
-
 SELECT departments.dept_name AS 'Department Name', CONCAT(employees.first_name, ' ', employees.last_name) AS Department_Manager, salaries.salary AS 'Salary'
 FROM departments
 JOIN dept_emp on departments.dept_no = dept_emp.dept_no
